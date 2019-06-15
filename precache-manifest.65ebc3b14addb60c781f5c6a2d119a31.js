c2142178fbe066f3f586bb27b6fa6779abd4ab59
KonstVolkovnitsky <konst.volkovnitsky@gmail.com>
2019-06-15 17:27:23 +0300

    Updates

---
+++ precache-manifest.65ebc3b14addb60c781f5c6a2d119a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecca075923b6b3c0b47db4af3fbc978f",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "24c1b78df8446a30194a",
+    "url": "/react_static-list-of-posts/static/css/main.d8e7f434.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "24c1b78df8446a30194a",
+    "url": "/react_static-list-of-posts/static/js/main.77a9a497.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
